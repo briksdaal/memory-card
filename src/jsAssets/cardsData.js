abe8f3c62b52c64a76ffbb5cfe891cbf6fcbe239
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-05-22 00:31:16 +0300

    Add image links to card data objects

--- src/jsAssets/cardsData.js
+++ src/jsAssets/cardsData.js
@@ -1,47 +1,165 @@
-export default [
+import android17 from '../assets/android_17.jpg';
+import android18 from '../assets/android_18.jpg';
+import bardock from '../assets/bardock.jpg';
+import bulma from '../assets/bulma.jpg';
+import buu from '../assets/buu.jpg';
+import cell from '../assets/cell.jpg';
+import chichi from '../assets/chichi.jpg';
+import dende from '../assets/dende.jpg';
+import frieza from '../assets/frieza.jpg';
+import futureTrunks from '../assets/future_trunks.jpg';
+import ginyu from '../assets/ginyu.jpg';
+import gohan from '../assets/gohan.jpg';
+import goku from '../assets/goku.jpg';
+import goten from '../assets/goten.jpg';
+import kami from '../assets/kami.jpg';
+import kidTrunks from '../assets/kid_trunks.jpg';
+import kingKai from '../assets/king_kai.jpg';
+import krillin from '../assets/krillin.jpg';
+import launch from '../assets/launch.jpg';
+import mrPopo from '../assets/mr_popo.jpg';
+import mrSatan from '../assets/mr_satan.jpg';
+import nappa from '../assets/nappa.jpg';
+import oxKing from '../assets/ox_king.jpg';
+import piccolo from '../assets/piccolo.jpg';
+import puar from '../assets/puar.jpg';
+import raditz from '../assets/raditz.jpg';
+import roshi from '../assets/roshi.jpg';
+import tien from '../assets/tien.jpg';
+import vegeta from '../assets/vegeta.jpg';
+import videl from '../assets/videl.jpg';
+import yajirobe from '../assets/yajirobe.jpg';
+import yamcha from '../assets/yamcha.jpg';
+
+const dbzCharacters = [
   {
-    name: 1,
+    name: 'Android 17',
+    img_url: android17,
   },
   {
-    name: 2,
+    name: 'Android 18',
+    img_url: android18,
   },
   {
-    name: 3,
+    name: 'Bardock',
+    img_url: bardock,
   },
   {
-    name: 4,
+    name: 'Bulma',
+    img_url: bulma,
   },
   {
-    name: 5,
+    name: 'Buu',
+    img_url: buu,
   },
   {
-    name: 6,
+    name: 'Cell',
+    img_url: cell,
   },
   {
-    name: 7,
+    name: 'Chi-Chi',
+    img_url: chichi,
   },
   {
-    name: 8,
+    name: 'Dende',
+    img_url: dende,
   },
   {
-    name: 9,
+    name: 'Frieza',
+    img_url: frieza,
   },
   {
-    name: 10,
+    name: 'Future Trunks',
+    img_url: futureTrunks,
   },
   {
-    name: 11,
+    name: 'Ginyu',
+    img_url: ginyu,
   },
   {
-    name: 12,
+    name: 'Gohan',
+    img_url: gohan,
   },
   {
-    name: 13,
+    name: 'Goku',
+    img_url: goku,
   },
   {
-    name: 14,
+    name: 'Goten',
+    img_url: goten,
   },
   {
-    name: 15,
+    name: 'Kami',
+    img_url: kami,
+  },
+  {
+    name: 'Kid Trunks',
+    img_url: kidTrunks,
+  },
+  {
+    name: 'King Kai',
+    img_url: kingKai,
+  },
+  {
+    name: 'Krillin',
+    img_url: krillin,
+  },
+  {
+    name: 'Launch',
+    img_url: launch,
+  },
+  {
+    name: 'Mr. Popo',
+    img_url: mrPopo,
+  },
+  {
+    name: 'Mr. Satan',
+    img_url: mrSatan,
+  },
+  {
+    name: 'Nappa',
+    img_url: nappa,
+  },
+  {
+    name: 'Ox-King',
+    img_url: oxKing,
+  },
+  {
+    name: 'Piccolo',
+    img_url: piccolo,
+  },
+  {
+    name: 'Puar',
+    img_url: puar,
+  },
+  {
+    name: 'Raditz',
+    img_url: raditz,
+  },
+  {
+    name: 'Roshi',
+    img_url: roshi,
+  },
+  {
+    name: 'Tien Shinhan',
+    img_url: tien,
+  },
+  {
+    name: 'Vegeta',
+    img_url: vegeta,
+  },
+  {
+    name: 'Videl',
+    img_url: videl,
+  },
+  {
+    name: 'Yajirobe',
+    img_url: yajirobe,
+  },
+  {
+    name: 'Yamcha',
+    img_url: yamcha,
   },
 ];
+
+export default dbzCharacters;
