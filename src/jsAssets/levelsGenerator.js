060ad09a223620d38e5217d51c7020f4af651670
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-05-22 00:29:42 +0300

    Change max cards to 16

--- src/jsAssets/levelsGenerator.js
+++ src/jsAssets/levelsGenerator.js
@@ -2,15 +2,9 @@ import { shuffle } from './helpers';
 import cardsData from './cardsData';
 
 export default function getLevelData(level) {
-  let noOfCards = level * 2;
-
-  if (noOfCards > cardsData.length) {
-    noOfCards = cardsData.length;
-
-    if (noOfCards % 2 === 1) {
-      noOfCards -= 1;
-    }
-  }
+  const noOfCards = level * 2 > 16
+    ? 16
+    : level * 2;
 
   return shuffle(cardsData).slice(0, noOfCards);
 }
